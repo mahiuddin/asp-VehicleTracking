cb9f3641df71f0474253cd1b63de7e94ce19e8e0
mahiuddin <mahi.ewu@gmail.com>
2021-05-10 15:06:04 +0800

    Add project files.

---
+++ VehicleTracking.DataAccess/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleTracking.DataAccess
+{
+    
+
+    public class DbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+        }
+        //public DbSet<UserType> UserType { get; set; }
+        //public DbSet<User> User { get; set; }
+        //public DbSet<Vehicle> Vehicle { get; set; }
+        //public DbSet<Device> Device { get; set; }
+        //public DbSet<Location> Location { get; set; }
+        
+    }
+}
